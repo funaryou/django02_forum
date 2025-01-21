c9f525e1bab0f45bb98f864e201844da31e2996e
funaryou <funahashi.ryouma@gmail.com>
2025-01-21 16:02:53 +0900

    create auth

--- django/dir/myproject/myproject/urls.py
+++ django/dir/myproject/myproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('forum_app/', include("forum_app.urls")),
+    path('accounts/', include("accounts.urls")), #認証機能へのルーティング
 ]

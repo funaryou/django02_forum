64f00cd8686ec15572d8315ea366bdef4e2677c9
funaryou <funahashi.ryouma@gmail.com>
2025-01-28 13:42:40 +0900

    comit

---
+++ django/dir/myproject/forum_app/forms.py
@@ -0,0 +1,8 @@
+# forum_app/forms.py
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'author']
--- django/dir/myproject/forum_app/views.py
+++ django/dir/myproject/forum_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect,get_object_or_404
 
 # Create your views here.
+#myapp/views.py
+from django.http import HttpResponse
+
+
+
+from .models import Post
+from . import views
+from .forms import PostForm
+
+# from django.contrib.auth.decorators import login_required
+
+# def index(request):
+#     return render(request, 'index.html')  # テンプレートを適宜変更
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html',{'posts':posts})
+
+
+def post_detail(request,pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post_detail.html',{'post':post})
